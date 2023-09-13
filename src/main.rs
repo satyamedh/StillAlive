fe5ead900c515dcf3abecd1fa42c47d0bcc4fd6b
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2023-09-13 16:25:03 +0530

    ... and have it colored!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::{thread, time::Duration};
 use clearscreen;
+use colored::Colorize;
 
 fn main() {
-    println!("Hello, world! This will go in 2 ");
-    thread::sleep(Duration::from_millis(4000));
     clearscreen::clear().expect("Somethin went wrong");
-    println!("HI! YOU SAW NOTHIN");
+    println!("{}", "--------------------------------------------------------".yellow());
 }
