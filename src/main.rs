a3644d1b1d0809410d619de8af209b8341ece971
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2023-09-13 17:00:14 +0530

    ... And it's padded accordingly :)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::{thread, time::Duration};
 use clearscreen;
-use colored::Colorize;
+use colored::{ColoredString, Colorize};
 
 fn main() {
+    let top_box_line: ColoredString = String::from("------------------------------------------------------------").yellow();
+
     clearscreen::clear().expect("Somethin went wrong");
-    println!("{}", "--------------------------------------------------------".yellow());
+    println!("{}", top_box_line);
+    let mut to_be_printed: String = String::new();
+    to_be_printed = format!("| {: <56} |", "Should be yellow n padded");
+    println!("{}", to_be_printed.yellow());
 }
