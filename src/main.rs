3673746fe96cd83c98ed41ebfd6d6c28a0ddb842
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2023-09-13 17:09:23 +0530

    ...And I can print lines from text :D

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::{thread, time::Duration};
 use clearscreen;
 use colored::{ColoredString, Colorize};
+use std::env;
+use std::fs;
 
 fn main() {
     let top_box_line: ColoredString = String::from("------------------------------------------------------------").yellow();
 
     clearscreen::clear().expect("Somethin went wrong");
     println!("{}", top_box_line);
-    let mut to_be_printed: String = String::new();
-    to_be_printed = format!("| {: <56} |", "Should be yellow n padded");
-    println!("{}", to_be_printed.yellow());
+
+
+    let contents = fs::read_to_string("src/left1_pg1.txt")
+        .expect("NU SMTH BROKE!");
+
+    let text = contents.split("\n");
+
+    for line in text {
+        let mut to_be_printed: String = String::new();
+        to_be_printed = format!("| {: <56} |", line);
+        println!("{}", to_be_printed.yellow());
+    }
 }
