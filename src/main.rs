329c8b78b58e5c232cf4f7dc462de269678047a2
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2023-09-13 17:55:39 +0530

    This is a very crude version and doesn't even type, but the text displays!

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,40 @@ use std::{thread, time::Duration};
 use clearscreen;
 use colored::{ColoredString, Colorize};
 use std::env;
+use std::fmt::format;
 use std::fs;
 
 fn main() {
-    let top_box_line: ColoredString = String::from("------------------------------------------------------------").yellow();
+    let box_vert_line: ColoredString = String::from("------------------------------------------------------------").yellow();
 
-    clearscreen::clear().expect("Somethin went wrong");
-    println!("{}", top_box_line);
+    let files_left = [
+        "left1_pg1.txt",
+        "left1_pg2.txt",
+        "left1_pg3.txt",
+        "left1_pg4.txt",
+    ];
 
+    for file in files_left {
 
-    let contents = fs::read_to_string("src/left1_pg1.txt")
-        .expect("NU SMTH BROKE!");
+        clearscreen::clear().expect("Loop issues...?");
 
-    let text = contents.split("\n");
+        println!("{}", box_vert_line);
+        let contents = fs::read_to_string(format!("src/{}", file))
+            .expect("NU SMTH BROKE!");
 
-    for line in text {
-        let mut to_be_printed: String = String::new();
-        to_be_printed = format!("| {: <56} |", line);
-        println!("{}", to_be_printed.yellow());
+        let text = contents.split("\n");
+
+        for line in text {
+            let mut to_be_printed: String = String::new();
+            to_be_printed = format!("| {: <56} |", line);
+            println!("{}", to_be_printed.yellow());
+        }
+
+        println!("{}", box_vert_line);
+
+        // Sleep 2s
+        thread::sleep(Duration::from_secs(2));
+
     }
+
 }
