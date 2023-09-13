abb4f8f0ff74dd2375b0d7a2049d41d3d765c171
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2023-09-13 16:20:25 +0530

    I can clear screen now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::{thread, time::Duration};
+use clearscreen;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! This will go in 2 ");
+    thread::sleep(Duration::from_millis(4000));
+    clearscreen::clear().expect("Somethin went wrong");
+    println!("HI! YOU SAW NOTHIN");
 }
